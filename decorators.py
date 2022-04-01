9f9adce683536a0b9ddf5c41a5f8ec273e47b293
AndrewMZ <andreika3981@gmail.com>
2022-04-01 21:51:26 +0700

    Create decorators.py

---
+++ decorators.py
@@ -0,0 +1,64 @@
+# define outer function
+def outerfunc(inputfunc):
+    '''
+        documentation string
+    '''
+    print('this is outer function')
+    outerVar = 'outerVar'
+    
+    def innerfunc():
+        print('this is executed before input function')
+        inputfunc()
+        print('this is inner function')
+        innerVar = 'innerVar'
+
+    return innerfunc
+
+# the function will be passed to outer function as an argument
+def inputfunc():
+    print('Hi I am input function')
+
+# the function decoratedFunc is "decorated" with the outer function
+@outerfunc
+def decoratedFunc():
+    print('Hi I am decorated function')
+
+
+# Testing the result
+print('------ someVar = outerfunc(somefunc) -------', end = '\n\n')
+someVar = outerfunc(inputfunc)
+print('\n')
+print('------ someVar() -------', end = '\n\n')
+someVar()
+print('\n')
+print('------ outerfunc(somefunc) -------', end = '\n\n')
+outerfunc(inputfunc)
+print('\n')
+print('------ decoratedFunc() -------', end = '\n\n')
+decoratedFunc()
+
+# Cosole Output
+# this is outer function
+# ------ someVar = outerfunc(somefunc) -------
+
+# this is outer function
+
+
+# ------ someVar() -------
+
+# this is executed before input function
+# Hi I am input function
+# this is inner function
+
+
+# ------ outerfunc(somefunc) -------
+
+# this is outer function
+
+
+# ------ decoratedFunc() -------
+
+# this is executed before input function
+# Hi I am decorated function
+# this is inner function
+# [Finished in 136ms]
