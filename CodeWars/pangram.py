9926b99f25e5cb41ec2e3474579d5808be85646b
Andrew <andreika3981@gmail.com>
2022-04-07 14:00:16 +0700

    interesting pangram task

---
+++ CodeWars/pangram.py
@@ -0,0 +1,26 @@
+# A pangram is a sentence that contains every single letter of the alphabet at least once. 
+# For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, 
+# because it uses the letters A-Z at least once (case is irrelevant).
+
+# Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
+
+a = 'hello this is my python testing sentence, and it is definitely not a pangram!'
+b = "The quick, brown fox jumps over the lazy dog!"
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def is_pangram(sentence: str):
+
+	words: list = sentence.split(' ')
+
+	for word in words:
+		letters :list = list(word)
+		for letter in letters:
+			if letter in alphabet:
+				alphabet.remove(letter)
+
+	return not bool(alphabet)
+
+print(is_pangram(a))
+# False
+print(is_pangram(b))
+# True
