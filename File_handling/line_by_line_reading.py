419d716f2690a362d70fce07381bb4efb113cb4e
AndrewMZ <andreika3981@gmail.com>
2022-07-11 15:29:52 +0700

    added context description

--- File_handling/line_by_line_reading.py
+++ File_handling/line_by_line_reading.py
@@ -1,3 +1,10 @@
+# context:
+#   the file 'real.txt' and 'imag.txt' are located in the same directory as the script
+#   txt files contain one column of float numbers in scientific notation with the size of 19201
+#   the last 19201st element is empty string ''
+#   the task is to create list of complex data comprised from I and Q data from files 'real.txt' and 'imag.txt'
+#   respectively
+
 with open('real.txt') as r, open('imag.txt') as i:
     r_data = 1
     L = list()
