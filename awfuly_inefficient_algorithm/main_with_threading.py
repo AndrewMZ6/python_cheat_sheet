35e1a78c236144f4cc339b84f02efee0ab84ad88
AndrewMZ <andreika3981@gmail.com>
2022-07-16 00:09:07 +0700

    removed unnecessary functions, used for loop

--- awfuly_inefficient_algorithm/main_with_threading.py
+++ awfuly_inefficient_algorithm/main_with_threading.py
@@ -6,29 +6,23 @@ from func_module import my_gen
 # def run_task_using_n_threads(n:int):
 # 	split the task to n threads
 
+
 L = []
 
-def thr1():
-	for i in my_gen(30):
-		L.append(f"t1::{i}")
-	print("thread 1 done!")
+def thr(n, name):
+	for i in my_gen(n):
+		L.append(f"{str(name + 1)} thread::{i}")
+	print(f"thread {name} is done!")
 
-def thr2():
-	for j in my_gen(30):
-		L.append(f"t2::{j}")
-	print("thread 2 done!")
+threads = []
+for _ in range(4):
+	t = Thread(target=thr, args=(25, _))
+	t.start()
+	threads.append(t)
 
-def main_thr():
-	for k in my_gen(40):
-		L.append(f"mt::{k}")
-	print("main thread done!")
 
-t1 = Thread(target=thr1)
-t2 = Thread(target=thr2)
-
-t1.start()
-t2.start()
-main_thr()
+for thread in threads:
+	thread.join()
 
 print(len(L))
 for i in L:
@@ -36,108 +30,109 @@ for i in L:
 
 
 # output:
-# thread 1 done!
-# thread 2 done!
-# main thread done!
+# thread 3 is done!
+# thread 2 is done!
+# thread 1 is done!
+# thread 0 is done!
 # 100
-# mt::blackautumn@example.net
-# t1::pacechristina@example.org
-# t2::mbradley@example.net
-# mt::kevin01@example.net
-# t1::robinyoung@example.com
-# t2::brownjason@example.net
-# mt::velasquezkristin@example.org
-# t2::rachel04@example.net
-# t1::vanderson@example.com
-# mt::darrell61@example.com
-# t2::brooke69@example.com
-# t1::xcarter@example.com
-# mt::ugonzales@example.net
-# t1::james57@example.org
-# t2::adamcarter@example.com
-# mt::bsmith@example.net
-# t1::dpatrick@example.net
-# t2::cody52@example.org
-# t1::victoria34@example.net
-# mt::zsalas@example.com
-# t2::jgriffin@example.com
-# mt::danajohnson@example.com
-# t1::gibbstracy@example.net
-# t2::brittneymaxwell@example.net
-# mt::reynoldslaura@example.net
-# t1::natasha60@example.com
-# t2::uritter@example.net
-# mt::wsmith@example.com
-# t1::rmartin@example.com
-# t2::mrivera@example.net
-# mt::vrivera@example.net
-# t1::kimberlyking@example.org
-# t2::katrinafuller@example.org
-# mt::kimashley@example.net
-# t1::christian14@example.com
-# t2::benjamin74@example.org
-# mt::brownsarah@example.net
-# t1::kwallace@example.com
-# t2::tuckersuzanne@example.com
-# mt::wintersnicole@example.com
-# t1::andersonscott@example.net
-# t2::dustingrimes@example.org
-# mt::ozimmerman@example.net
-# t1::stephanie65@example.com
-# t2::peggycole@example.org
-# mt::jacobtaylor@example.org
-# t1::christina19@example.com
-# t2::holly69@example.net
-# mt::cory64@example.net
-# t1::varnold@example.net
-# t2::lnichols@example.net
-# mt::floydcarrie@example.net
-# t1::stephen54@example.net
-# t2::patriciagreene@example.org
-# mt::tamara53@example.org
-# t1::hayley16@example.com
-# t2::ianwheeler@example.org
-# mt::hgallagher@example.com
-# t1::victoria00@example.net
-# t2::whiterobert@example.net
-# mt::david51@example.com
-# t1::haynesmichelle@example.com
-# t2::brettreid@example.com
-# mt::martinezsamantha@example.com
-# t1::nhendricks@example.com
-# t2::sdavis@example.com
-# mt::abean@example.org
-# t1::lsmith@example.org
-# t2::michaellambert@example.com
-# mt::juan80@example.org
-# t1::bruce70@example.org
-# t2::derek83@example.net
-# mt::jeffreygardner@example.org
-# t1::apham@example.net
-# t2::morenocaroline@example.net
-# mt::tbaker@example.com
-# t1::chase90@example.org
-# t2::randy96@example.com
-# t1::shane14@example.com
-# mt::princetyler@example.com
-# t2::adamsclaudia@example.com
-# mt::randerson@example.net
-# t1::burchlindsay@example.com
-# t2::dwyatt@example.net
-# mt::edwardjackson@example.org
-# t1::jeremiah36@example.com
-# t2::david86@example.org
-# mt::elizabethboyd@example.net
-# t1::katie52@example.org
-# t2::thomas59@example.net
-# mt::joshua76@example.net
-# mt::jameswalters@example.com
-# mt::kayla01@example.com
-# mt::sallyfuller@example.net
-# mt::lsimpson@example.com
-# mt::stewartraven@example.net
-# mt::dunnsheri@example.org
-# mt::travis10@example.net
-# mt::mmiller@example.com
-# mt::ronaldbrown@example.org
-# [Finished in 7.2s]
+# 4 thread::davidlloyd@example.org
+# 1 thread::byrdjeff@example.org
+# 3 thread::vickie64@example.net
+# 2 thread::andregarcia@example.net
+# 2 thread::christopher61@example.org
+# 4 thread::erikapena@example.net
+# 3 thread::ellischristy@example.org
+# 1 thread::graceolson@example.com
+# 4 thread::heatherwebb@example.net
+# 2 thread::alex93@example.org
+# 1 thread::kimberlymoore@example.net
+# 3 thread::randerson@example.net
+# 3 thread::jessicagutierrez@example.com
+# 4 thread::vvilla@example.com
+# 1 thread::evanscarol@example.org
+# 2 thread::jeffreywhite@example.org
+# 3 thread::qgaines@example.com
+# 4 thread::hollyburnett@example.com
+# 2 thread::watsondanny@example.com
+# 1 thread::jeffreylarsen@example.org
+# 3 thread::lindseyedward@example.com
+# 4 thread::harrisonnichole@example.org
+# 2 thread::lewisheather@example.net
+# 1 thread::garciajanet@example.org
+# 3 thread::stephanie00@example.com
+# 2 thread::jamesschwartz@example.org
+# 4 thread::hlopez@example.net
+# 1 thread::tyler10@example.net
+# 3 thread::matthew12@example.com
+# 4 thread::rodrigueznicholas@example.com
+# 2 thread::vwilson@example.net
+# 1 thread::monicaramos@example.com
+# 3 thread::mdelgado@example.net
+# 4 thread::stephaniespencer@example.org
+# 2 thread::xsmith@example.com
+# 1 thread::shellyzimmerman@example.net
+# 3 thread::justinjohnson@example.org
+# 4 thread::heatherarmstrong@example.com
+# 2 thread::richmondelizabeth@example.net
+# 1 thread::mcdanielheidi@example.org
+# 3 thread::rachel91@example.net
+# 4 thread::tperry@example.org
+# 2 thread::opeters@example.net
+# 1 thread::charlesbird@example.com
+# 3 thread::elizabethjones@example.com
+# 4 thread::igarcia@example.org
+# 2 thread::potterthomas@example.org
+# 1 thread::nancywilliams@example.org
+# 3 thread::mzamora@example.net
+# 4 thread::michaelberry@example.net
+# 2 thread::tracy23@example.org
+# 1 thread::thomas54@example.com
+# 3 thread::ricky20@example.net
+# 4 thread::johnsonkaren@example.net
+# 2 thread::hartbrandon@example.net
+# 1 thread::ubarnes@example.org
+# 3 thread::rdavis@example.org
+# 2 thread::anthony45@example.net
+# 4 thread::gonzaleschristopher@example.com
+# 1 thread::michael78@example.org
+# 3 thread::khandonald@example.org
+# 2 thread::vanessa02@example.com
+# 4 thread::joshua50@example.net
+# 1 thread::blanchardderrick@example.net
+# 3 thread::hurstsarah@example.net
+# 4 thread::pauladkins@example.org
+# 2 thread::frank96@example.net
+# 1 thread::echang@example.net
+# 3 thread::scottjones@example.com
+# 4 thread::hilljulie@example.com
+# 2 thread::greerwilliam@example.com
+# 1 thread::melissa45@example.org
+# 3 thread::morristeresa@example.net
+# 4 thread::kari32@example.net
+# 2 thread::cynthia70@example.com
+# 1 thread::kdiaz@example.net
+# 3 thread::millsbrooke@example.com
+# 4 thread::qreeves@example.org
+# 2 thread::wreyes@example.org
+# 1 thread::qwyatt@example.org
+# 4 thread::bradleyperez@example.com
+# 3 thread::evansrebecca@example.net
+# 2 thread::vperez@example.org
+# 1 thread::spencer07@example.net
+# 4 thread::justin58@example.net
+# 3 thread::eric74@example.org
+# 2 thread::znewman@example.org
+# 1 thread::fowlerdavid@example.net
+# 4 thread::zdean@example.com
+# 3 thread::robertsnyder@example.com
+# 2 thread::ijoseph@example.com
+# 1 thread::trosales@example.com
+# 4 thread::gdiaz@example.net
+# 3 thread::sotochristian@example.org
+# 2 thread::ylambert@example.com
+# 1 thread::danielmccormick@example.net
+# 4 thread::kingjeff@example.org
+# 3 thread::michaelrivera@example.org
+# 2 thread::michele98@example.com
+# 1 thread::rachel26@example.com
+# [Finished in 5.9s]
