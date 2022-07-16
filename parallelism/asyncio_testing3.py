9ed56979f25d14fb0894d3ba1dabdc8e4731d32b
Andrew <andreika3981@gmail.com>
2022-07-16 17:35:00 +0700

    added better naming and description

--- parallelism/asyncio_testing3.py
+++ parallelism/asyncio_testing3.py
@@ -1,22 +1,39 @@
 import asyncio
 import time
 
-
-async def f():
-	print('Hey, this is function f() is executed')
+# nonblocking function can ceeds workflow
+# to event loop even if it's not finished
+async def nonbocking():
+	print('nonblocking staring')
 	await asyncio.sleep(1)
-	print('Hey, this is function f() is executed')
+	print('nonblocking finished')
 
-async def g():
+# blocking function doesn't let workflow
+# go anywhere untill it's finished
+async def blocking():
+	print('blocking staring')
 	for i in range(5):
-		print(f'counting {i}')
+		print(f'bl -> {i}')
 		time.sleep(1)
-		# await asyncio.sleep(.3)
+	print('blocking finished')
 
 
 async def main():
-	await asyncio.gather(f(), g())
+	await asyncio.gather(nonbocking(), blocking())
 
 
 if __name__ == '__main__':
 	asyncio.run(main())
+
+
+# output:
+# nonblocking staring
+# blocking staring
+# bl -> 0
+# bl -> 1
+# bl -> 2
+# bl -> 3
+# bl -> 4
+# blocking finished
+# nonblocking finished
+# [Finished in 5.3s]
