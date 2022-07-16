b4c0b26a885b08894fdf39fde772e12810269aa7
AndrewMZ <andreika3981@gmail.com>
2022-07-16 12:36:20 +0700

    added blocking function

--- parallelism/asyncio_testing2.py
+++ parallelism/asyncio_testing2.py
@@ -7,73 +7,41 @@ import random
 import string as s
 import asyncio
 from sys import exit
+from time import sleep
 
+async def blocking():
+	for i in range(10):
+		print(i)
+		sleep(.1)
+
+# the function "numbers" suspends and awaits for function "blocking" to finish
+# then the execution proceeds
 async def numbers():
-	for num in range(20):
-		print(num)
-		await asyncio.sleep((1+random.random())**2 - 1)
-	return 'numbers finished successfully'
-
-async def alpha():
-	for letter in s.ascii_lowercase:
-		print(letter)
-		await asyncio.sleep((1+random.random())**2 - 1)
+	print("----------------1----------")
+	res = await blocking()
+	print("----------------2----------")
 	return 'numbers finished successfully'
 
 async def main():
-	res = await asyncio.gather(numbers(), alpha())
+	res = await asyncio.gather(numbers())
 	print(res)
 
 if __name__ == '__main__':
 	asyncio.run(main())
 
-# Output:
-# 
+
+# output:
+# ----------------1----------
 # 0
-# a
-# b
 # 1
-# c
 # 2
 # 3
-# d
 # 4
-# e
-# f
 # 5
-# g
-# h
 # 6
 # 7
-# i
-# j
-# k
-# l
 # 8
-# m
 # 9
-# 10
-# 11
-# n
-# o
-# 12
-# 13
-# p
-# 14
-# q
-# 15
-# r
-# 16
-# s
-# 17
-# t
-# u
-# 18
-# v
-# 19
-# w
-# x
-# y
-# z
-# ['numbers finished successfully', 'numbers finished successfully']
-# [Finished in 37.9s]
+# ----------------2----------
+# ['numbers finished successfully']
+# [Finished in 1.3s]
