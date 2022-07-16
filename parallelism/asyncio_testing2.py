5dbdfcf32010f647b1b3fde7dc700239fe77fdd6
Andrew <andreika3981@gmail.com>
2022-07-16 14:53:44 +0700

    changed naming to more readable

--- parallelism/asyncio_testing2.py
+++ parallelism/asyncio_testing2.py
@@ -11,29 +11,29 @@ async def email_gen(x):
 		yield emails.email()
 
 # asynchronous random number [0.0, 1.0) generator
-async def mygen(x):
-	print('-> mygen first line')
+async def num_gen(x):
+	print('-> num_gen first line')
 	while x:
 		x -= 1
 		yield random.random()
 
 # prints random numbers in async for loop
-async def f(x):
-	print('-> f() first line')
-	async for i in mygen(x):
+async def numbers(x):
+	print('-> numbers function first line')
+	async for i in num_gen(x):
 			print(i)
 			await asyncio.sleep(1)
 
 # prints emails in async for loop
-async def h(x):
-	print('-> h() first line')
+async def emails(x):
+	print('-> emails function first line')
 	async for j in email_gen(x):
 			print(j)
 			await asyncio.sleep(1)
 
 # main coroutine
 async def main():
-	await asyncio.gather(f(4), h(3))
+	await asyncio.gather(numbers(4), emails(3))
 
 # creates eventloop and runs the main coroutine
 if __name__ == '__main__':
