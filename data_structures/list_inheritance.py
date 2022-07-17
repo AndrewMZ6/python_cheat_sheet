bbf74de73718d4f0e90060889de49f14bc24ac29
Andrew <andreika3981@gmail.com>
2022-07-17 13:12:05 +0700

    removed unnecessary prints, added f-strings

--- data_structures/list_inheritance.py
+++ data_structures/list_inheritance.py
@@ -2,19 +2,20 @@ class unique_list(list):
 	pass
 
 L1 = [1, 8.1, 9, 52.3, -6.1, 0, -90, 4, 52.3, 3.14, 3.14]  # len(L1) == 11
-
+print(f"original list:\n{L1}")
 S1 = set(L1)
 new_list = list(S1)
-print(new_list)
+print(f"new_list:\n{new_list}")
+
 # output:
 # [0, 1, 3.14, 4, -90, 8.1, 9, 52.3, -6.1]
 
 
 # Problem:
 # elements of new list are not in the same order as 
-# were in original list
+# were in original list L1
 
-# Algorithm
+# Algorithm:
 # order = dict()
 # for element in new_list:
 # 	ind = check what was the index of the element in the original list
@@ -37,15 +38,14 @@ for element in new_list:
 	if element not in order:
 		order[element] = L1.index(element)
 
-print(order)
-print(len(order), len(new_list))
+print(f"order dict:\n{order}")
+
 
 corrected_list = ['x']*len(L1)
-print(corrected_list)
+
 
 for value, index in order.items():
-	print(index, value)
 	corrected_list[index] = value
 
 corrected_list = list(filter(lambda x: not isinstance(x, str), corrected_list))
-print(corrected_list)
+print(f"corrected_list:\n{corrected_list}")
