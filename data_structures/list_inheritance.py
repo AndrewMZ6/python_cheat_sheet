4a6a026e93d3ad1ba1a1690d7df9c4e3eac9b0e9
Andrew <andreika3981@gmail.com>
2022-07-19 19:45:53 +0700

    added some description

--- data_structures/list_inheritance.py
+++ data_structures/list_inheritance.py
@@ -8,7 +8,6 @@
 
 import sys
 class unique_list(list):
-	print(__doc__)
 	def __init__(self, x):
 		super().__init__(x)
 		# print(f"""x:\n
@@ -39,12 +38,15 @@ class unique_list(list):
 			if element not in order:
 				order[element] = L1.index(element)
 
+		
 		corrected_list = ['x']*len(L1)
 
 
 		for value, index in order.items():
 			corrected_list[index] = value
 
+		# the problem with this filter is that if original list L1 contains str values
+		# then they will be filtered. That's not good. Yes
 		corrected_list = list(filter(lambda x: not isinstance(x, str), corrected_list))
 
 		# check if the correction is right:
@@ -62,40 +64,15 @@ class unique_list(list):
 		return corrected_list
 
 
-L1 = [1, 8.1, 9, 52.3, -6.1, 0, -90, 4, 52.3, 3.14, 3.14]  # add 'x' element that will be removed
+L1 = [1, 8.1, 9, 52.3, -6.1, 0, -90, 4, 52.3, 3.14, 3.14] 
 
-print(unique_list(L1).make_unique())
+print(f"orginal list:\n{L1}")
+print(f"Uniquefied list:\n{unique_list(L1).make_unique()}")
 
 
 # OUTPUT:
-# x:
-
-# 				  [1, 8.1, 9, 52.3, -6.1, 0, -90, 4, 52.3, 3.14, 3.14, 'x']
-
-# 				  type:
-
-# 				  <class 'list'>
-
-# 				  dir:
-
-# 				  ['__add__', '__class__', '__class_getitem__', '__contains__', '__delattr__', '__delitem__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__getitem__', '__gt__', '__hash__', '__iadd__', '__imul__', '__init__', '__init_subclass__', '__iter__', '__le__', '__len__', '__lt__', '__mul__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__reversed__', '__rmul__', '__setattr__', '__setitem__', '__sizeof__', '__str__', '__subclasshook__', 'append', 'clear', 'copy', 'count', 'extend', 'index', 'insert', 'pop', 'remove', 'reverse', 'sort']
-
-# self:
-
-# 				  [1, 8.1, 9, 52.3, -6.1, 0, -90, 4, 52.3, 3.14, 3.14, 'x']
-
-# 				  type:
-
-# 				  <class '__main__.unique_list'>
-
-# 				  dir:
-
-# 				  ['__add__', '__class__', '__class_getitem__', '__contains__', '__delattr__', '__delitem__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__getitem__', '__gt__', '__hash__', '__iadd__', '__imul__', '__init__', '__init_subclass__', '__iter__', '__le__', '__len__', '__lt__', '__module__', '__mul__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__reversed__', '__rmul__', '__setattr__', '__setitem__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', 'append', 'clear', 'copy', 'count', 'extend', 'index', 'insert', 'make_unique', 'pop', 'remove', 'reverse', 'sort']
-
-# Traceback (most recent call last):
-#   line 60, in <module>
-#     print(unique_list(L1).make_unique())
-#   line 53, in make_unique
-#     assert set(self) == set(corrected_list), "The number of unique elements does not match"
-# AssertionError: The number of unique elements does not match
-# [Finished in 152ms]
+# orginal list:
+# [1, 8.1, 9, 52.3, -6.1, 0, -90, 4, 52.3, 3.14, 3.14]
+# Uniquefied list:
+# [1, 8.1, 9, 52.3, -6.1, 0, -90, 4, 3.14]
+# [Finished in 234ms]
