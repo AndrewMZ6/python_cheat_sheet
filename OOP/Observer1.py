84f49c408e6dc542ac7387e617ad867dddcc4bd0
Andrew <andreika3981@gmail.com>
2022-11-25 23:07:53 +0700

    added basic implementation of the design pattern

---
+++ OOP/Observer1.py
@@ -0,0 +1,89 @@
+from abc import ABC, abstractmethod
+
+
+class Observer(ABC):
+
+	@abstractmethod
+	def update(self):
+		"""Reaction to subject notifications"""
+
+
+class Subject(ABC):
+
+	@abstractmethod
+	def attach(self, observer: Observer):
+		"""Add new observer to observers list"""
+
+	@abstractmethod
+	def detach(self, observer: Observer):
+		"""Remove observer to observers list"""
+
+	@abstractmethod
+	def notify(self):
+		"""Notify every observer in observers list"""
+
+
+class concrete_Observer_1(Observer):
+	def __init__(self, name: str) -> None:
+		self.name = name
+		
+	def update(self) -> None:
+		print(f"-> {self.name} sees the notification!")
+
+
+class Publisher(Subject):
+	def __init__(self) -> None:
+		self.observer_list: list[Observer] = []
+		self._notification_counter = 0
+
+	def attach(self, observer: Observer) -> None:
+		if observer not in self.observer_list:
+			self.observer_list.append(observer)
+
+	def detach(self, observer: Observer) -> None:
+		self.observer_list.remove(observer)
+
+	def notify(self) -> None:
+
+		if self.observer_list:
+			self._notification_counter += 1
+			print(f'Notification # {self._notification_counter}. {self.__class__.__name__} notifies observers')
+			for observer in self.observer_list:
+				observer.update()
+		else:
+			print('The observer list is empty. Nobody\'s interested :(')
+
+
+
+if __name__ == '__main__':
+	Ivan = concrete_Observer_1('Ivan')
+	Masha = concrete_Observer_1('Masha')
+	Georgy = concrete_Observer_1('Georgy')
+	termometer = Publisher()
+
+	termometer.attach(Ivan)
+	termometer.notify()
+
+	termometer.attach(Masha)
+	termometer.notify()
+
+	termometer.detach(Masha)
+	termometer.notify()
+
+	termometer.attach(Georgy)
+	termometer.notify()
+
+	termometer.detach(Ivan)
+	termometer.notify()
+	termometer.notify()
+
+	termometer.detach(Georgy)
+	termometer.notify()
+	termometer.notify()
+
+
+	termometer.attach(Ivan)
+	termometer.notify()
+
+	termometer.attach(Masha)
+	termometer.notify()
