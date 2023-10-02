cd70a0ea02091cb6f7bfd88513acb6544fcf6b48
Andrew <andreika3981@gmail.com>
2023-10-02 19:24:14 +0700

    init parsing folder

---
+++ parsing/log_sorting.py
@@ -0,0 +1,27 @@
+import re
+from pathlib import Path
+
+SORTED_LOG_FILE_NAME = 'sorted_log_rssi.txt'
+LOG_FILE_NAME = 'log_rssi.txt'
+LOG_FILE_PATH = Path(__file__).parent/LOG_FILE_NAME
+SORTED_LOG_FILE_NAME = Path(__file__).parent/SORTED_LOG_FILE_NAME
+
+
+SEARCH_TAG = 'DnRSSIdBm'
+PATTERN = fr'{SEARCH_TAG} = (-?\d+)'
+
+with open(LOG_FILE_NAME) as f:
+	# while True:
+	string = f.read()
+	parsing_item = re.findall(PATTERN, string)
+	# if parsing_item:
+	# 	print(parsing_item.group())
+
+
+print(set(list(map(int, parsing_item))))
+
+
+
+
+# if __name__ == '__main__':
+# 	print(LOG_FILE_PATH)
